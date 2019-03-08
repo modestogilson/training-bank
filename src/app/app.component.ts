fdb6fbd30db4160803f4bbcd5d517b8130d8e1d6
Gilson Modesto <modestogilson@oi.net.br>
2019-03-08 15:21:07 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Template } from '../../node_modules/@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+
+  constructor(public dialog: MatDialog){
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(AppComponent, {
+      height: '350px'
+    });
+
+    dialogRef.afterClosed().subscribe( result => {
+      console.log('Dialog result: ${result}');
+    });
+  }
+
   title = 'training-bank';
 }
